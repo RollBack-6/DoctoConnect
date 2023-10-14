b27badd030807869a6b17de55bd90c276ef4978e
Tanishkachavan1 <Tanishkachavan02@gmail.com>
2023-10-14 18:52:31 +0530

    Doctor tab updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,46 @@
+import React, { useState } from 'react';
 import "./App.css";
-import Doctor from "./Components/Right_Column/Doctor/Doctor.jsx";
 import Selection from "./Components/LeftColumn/Selection";
+import RegistrationForm from "./Components/Right_Column/registerPatient/registerPatient.jsx";
+import SearchBar from "./Components/Right_Column/Overview/SearchBar.jsx";
+import Patient from "./Components/Right_Column/Patient/Patient.jsx"
 
 function App() {
+  const [showRegistrationForm, setShowRegistrationForm] = useState(false);
+  const [showOverview, setShowOverview] = useState(true);
+  const [showPatient, setShowPatient] = useState(false); // Set initial state to true
+
+  const toggleRegistrationForm = () => {
+    setShowRegistrationForm(true);
+    setShowOverview(false);
+    setShowPatient(false);
+  };
+
+  const toggleOverview = () => {
+    setShowRegistrationForm(false);
+    setShowOverview(true);
+    setShowPatient(false);
+  };
+
+  const togglePatient = () => {
+    setShowRegistrationForm(false);
+    setShowOverview(false);
+    setShowPatient(true);
+  };
   return (
     <>
       <div className="row">
         <div className="column left">
-          <Selection/>
+          <Selection 
+            toggleRegistrationForm={toggleRegistrationForm}
+            toggleOverview={toggleOverview}
+            togglePatient={togglePatient}
+          />
         </div>
         <div className="column right">
-          <Doctor/>
+          {showRegistrationForm && <RegistrationForm/>}
+          {showOverview && <SearchBar/>}
+          {showPatient && <Patient/>}
         </div>
       </div>
     </>
