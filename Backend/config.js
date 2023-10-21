c42a32c34ea8469aaffd651c1d3396e18cba53bb
Pratham-Gaikwad <prathamgaikwad2011@gmail.com>
2023-10-21 20:02:57 +0530

    Backend ka start

---
+++ Backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongodbURL="mongodb+srv://root:root@doctoconnect.qq9iqwv.mongodb.net/?retryWrites=true&w=majority";
---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+import express, { request, response } from "express";
+import { PORT, mongodbURL } from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Your app is working.");
+});
+
+
+mongoose
+  .connect(mongodbURL)
+  .then(() => {
+    console.log("App is connectec to the database.")
+    app.listen(PORT, () => {
+        console.log("Your app is working.");
+      });
+      
+  })
+  .catch((error) => {
+    console.log(error);
+  });
